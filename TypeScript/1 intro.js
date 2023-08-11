3f0dbdd0e71b48b7f971365c234d69e6644a4dc6
Homayoun <homayoun763@gmail.com>
2023-08-11 19:11:53 +0330

    Intorudction of the typescript

---
+++ TypeScript/1 intro.js
@@ -0,0 +1,26 @@
+//What is the typescript ?
+//A programming language to address shortcommings of JavaScript that created by microsoft
+//like brother or sister of the javascript 
+//javascript is like kid without any discipline and do anything he/she wants
+//Typescript is like kid with some discipline 
+
+//here some cool features of the typescript 
+//static typing 
+//we have two types of programming 
+//1 staticlly-type
+//Like C++ , C# , Java
+//we know the type of the varaiables at the compile time and coding
+
+//2 dynamically-type
+//Like python , javascript , ruby
+//the type of the variables are dynamic and chage in the run time 
+
+
+//TypeScript : is javascript with the Type checking also have
+//Code completion 
+//Refacoring 
+//Shorthand notations 
+
+//some drawback of the typescript 
+//compilation 
+//discipline in coding 
